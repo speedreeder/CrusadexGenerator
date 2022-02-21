b9d0470c65de271abc18936627e37436e3a4a71c
Alexander Reeder <alexgreeder@outlook.COM>
2022-02-21 13:14:50 -0500

    Added some defaults to the options

--- CrusadexGenerator/CrusadexGeneratorOptions.cs
+++ CrusadexGenerator/CrusadexGeneratorOptions.cs
@@ -2,13 +2,13 @@
 {
     public class CrusadexGeneratorOptions
     {
-        public int MaxWords { get; set; }
+        public int MaxWords { get; set; } = 5;
         public int MinWords { get; set; } = 1;
         public int? MaxTwoLetterWords { get; set; }
         public int? MaxThreeLetterWords { get; set; }
         public int? MaxCubeJoints { get; set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
+        public int Height { get; set; } = 10;
+        public int Width { get; set; } = 10;
         public int MinWordLength { get; set; } = 2;
         public int MaxWordLength { get; set; } = 7;
     }
